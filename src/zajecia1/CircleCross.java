88a7e008c776f285a7a21e447c3e2dfc068167ff
Rainy96 <jan.rejnowski@gmail.com>
2017-04-21 16:09:03 +0200

    Upload changes

--- src/zajecia1/CircleCross.java
+++ src/zajecia1/CircleCross.java
@@ -7,38 +7,56 @@ import java.util.Scanner;
  */
 public class CircleCross {
     public static void main(String[] args) {
-        char[][] gameBoard = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
-        displayBoard(gameBoard);
+        char[][] gameBoard = {{'1','2','3'}, {'4','5','6'}, {'7','8','9'}};
         Scanner scanner = new Scanner(System.in);
-        for(int i = 0; i < 9; i++){
-            System.out.println("Insert sign position");
-            int position = scanner.nextInt();
-//            int[] positions = convertPosition(position);
-//            gameBoard[positions[0]] [positions[1]] = i % 2 == 0 ? 'X' : 'O';
+        for (int i = 0; i < 9; i++) {
+            boolean inputFromUserFlag = false;
+            displayBoard(gameBoard);
+            int position = 0;
+            while(!inputFromUserFlag) {
+                System.out.println("Insert sign position.");
+                position = scanner.nextInt();
+                inputFromUserFlag = validatePositionFromUser(position, gameBoard);
+                if (!inputFromUserFlag){
+                    System.out.println("Wrong position. Insert again. ");
+                }
+            }
+            int[] positions = convertPosition(position);
+            gameBoard[positions[0]] [positions[1]] = (i % 2 == 0) ? 'X' : 'O';
         }
-        System.out.println("End of game!");
+        System.out.println("End of the game.");
         displayBoard(gameBoard);
-        int position = 0;
-//        while(inputFromUserFlag)
     }
 
-//    public static int[] convertPosition(int positionFromUser) {
-//        int[] position = new int[2];
-//        position[0] = (positionFromUser - 1) / 3;
-//        position[1] = (positionFromUser - 1) % 3;
-//    }
-
     public static void displayBoard(char[][] gameBoard){
-        for (int i = 0; i < gameBoard.length; i++) {
-            for (int j = 0; j < gameBoard[0].length; j++) {
+        for(int i = 0; i < gameBoard.length; i++){
+            for(int j = 0; j < gameBoard[0].length; j++){
                 System.out.print(gameBoard[i][j]);
                 if(j != gameBoard[0].length - 1){
                     System.out.print("|");
                 }
             }
-            if(i != gameBoard.length - 1) {
-                System.out.println("\n-----\n");
+            if(i != gameBoard.length - 1){
+                System.out.print("\n-----\n");
             }
         }
+        System.out.println();
+    }
+
+    public static int[] convertPosition(int positionFromUser) {
+        int[] position = new int[2];
+        position[0] = (positionFromUser - 1) / 3;
+        position[1] = (positionFromUser - 1) % 3;
+        return position;
+    }
+
+    public static boolean isFieldEmpty(char[][] gameBoard, int i, int j) {
+        return !(gameBoard[i][j] == 'X' || gameBoard[i][j] == 'O');
+    }
+
+    public static boolean validatePositionFromUser(int positionFromUser, char[][] gameBoard) {
+        int[] ints = convertPosition(positionFromUser);
+        return (positionFromUser > 0 && positionFromUser < 10 &&
+                isFieldEmpty(gameBoard, ints[0], ints[1]));
     }
 }
--- src/zajecia1/Zajecia10.java
+++ src/zajecia1/Zajecia10.java
@@ -7,109 +7,154 @@ import java.util.Random;
  */
 public class Zajecia10 {
     public static void main(String[] args) {
-//        String message = "Ala ma kota";
-//        System.out.println(find(message, 'm'));
-//        printStringStatistics(stringStatistics("Ala ma kota i borsuka"));
+//        System.out.println(find("Ala ma kota", 'm'));
+//        System.out.println(find("Ala ma kota", 'k'));
+//        System.out.println(find("Ala ma kota", 'z'));
+//        System.out.println(countAll("Ala ma kota", 'a'));
+//        System.out.println(countAll("Ala ma kota", 'm'));
+//        System.out.println(countAll("Ala ma kota", 'z'));
+//        Zajecia4.displayArray(stringStatistics("aaabbbcccdddzzz"));
+//        int[] statistics = stringStatistics("aabbbbbcceezzz");
+//        int[] statistics = stringStatistics("Ala ma kota");
+//        int[] array = {1, 2, 3, 4, 5, 6, 7, 8};
+//        printStringStatistics(statistics);
+//        printStringStatistics(array);
+//        System.out.println(multiplyDigits(123));
+//        System.out.println(multiplyDigits(1234));
+//        System.out.println(multiplyDigits(12345));
+        int[][] ints = randomMatrix(3, 10, -5);
+        System.out.println(avg(ints));
+        double[][] avg = avg(randomMatrix(3, 10, -5), randomMatrix(3, 15, 5));
     }
 
-//    public static int find(String message, char sentence){
-//        int index = 0;
-//        for(int i = 0; i < message.length(); i++){
-//            if(message.charAt(i) == sentence){
-//                index += i;
-//            }
-//        }
-//        return index;
-//    }
-    public static int find(String message, char sentence){
+    //01234
+    //Ala ma kota, m -> 4
+    public static int find(String message, char sentence) {
         char[] charArray = message.toCharArray();
         int i = 0;
-        while( i < charArray.length && charArray[i] != sentence){
+        while (i < charArray.length && charArray[i] != sentence) {
             i++;
         }
-        return i == charArray.length ? -1 : i;
+        return (i == charArray.length) ? -1 : i;
     }
 
-    public static int countAll(String message, char sentence){
+    //Ala ma kota, a -> 3
+    public static int countAll(String message, char sentence) {
         char[] charArray = message.toCharArray();
         int counter = 0;
-        for(int i = 0; i < charArray.length; i++){
-            if(toLowerCase(charArray[i]) == toLowerCase(sentence)){
+        for (int i = 0; i < charArray.length; i++) {
+            if (toLowerCase(charArray[i]) == toLowerCase(sentence)) {
                 counter++;
             }
         }
         return counter;
     }
 
-    public static char toLowerCase(char a){
-        if(a >= 65 && a <= 90){
+    public static char toLowerCase(char a) {
+        if (a >= 65 && a <= 90) {
             a += 32;
         }
         return a;
     }
 
+    //aabbbbbccee -> {2, 4, 2, 0, 2, ...}
+    //Ala ma kota i borsuka -> {5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, ...}
     public static int[] stringStatistics(String message) {
         char[] charArray = message.toCharArray();
         int[] ints = new int[26];
-        for (int i = 0; i < charArray.length ; i++) {
+        for (int i = 0; i < charArray.length; i++) {
             char charInLowerCase = toLowerCase(charArray[i]);
-            if(charInLowerCase >= 97 && charInLowerCase <= 122){
+            if (charInLowerCase >= 97 && charInLowerCase <= 122) {
                 ints[charInLowerCase - 97]++;
             }
         }
         return ints;
     }
 
-    public static void printStringStatistics(int[] array){
-        for (int i = 0; i < array.length ; i++) {
+    //{2, 4, 2, 0, 2, ...} -> aabbbbbccee
+    public static void printStringStatistics(int[] array) {
+//        String message = "";
+        for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i]; j++) {
-                System.out.print((char)i + 97);
+                System.out.print((char)(i + 97));
+//                message += (char) (i + 97);
             }
         }
+//        System.out.println(message);
+//        return message;
     }
-
+    //123 -> 1 * 2 * 3 = 6
     public static int multiplyDigits(int number) {
         int product = 1;
-        while(number != 0){
+        while (number != 0) {
             product *= number % 10;
             number /= 10;
         }
         return product;
     }
-
-    public static int[][] randomMatrix(int size, int bounds, int offset){
-        Random random = new Random();
-        int[][] resultMatrix = new int[size][size];
-        for(int i = 0; i < size; i++){
-            for(int j = 0; j < size; j++){
-                resultMatrix[i][j] = random.nextInt(bounds) + offset;
-            }
-        }
-        return resultMatrix;
-    }
-
-    public static double avg(int[][] matrix){
+    public static double avg(int[][] matrix) {
         int sum = sum(matrix);
-        return sum / (double)(matrix.length * matrix[0].length);
+        return sum /(double)(matrix.length * matrix[0].length);
     }
-
-    public static int sum (int[][] matrix){
+    public static int sum(int[][] matrix) {
         int sum = 0;
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix[0].length; j++){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
                 sum += matrix[i][j];
             }
         }
         return sum;
     }
 
-    public static double[][] avg(int[][] matrix1, int[][] matrix2){
+    public static double[][] avg(int[][] matrix1, int[][] matrix2) {
         double[][] resultMatrix = new double[matrix1.length][matrix1[0].length];
-        for(int i = 0; i < matrix1.length; i++){
-            for(int j = 0; j < matrix1[0].length; j++){
+        for (int i = 0; i < matrix1.length; i++) {
+            for (int j = 0; j < matrix1[0].length; j++) {
                 resultMatrix[i][j] = (matrix1[i][j] + matrix2[i][j]) / 2.0;
             }
         }
         return resultMatrix;
+    }
+
+    public static int[][] randomMatrix(int size, int bounds, int offset) {
+        Random random = new Random();
+        int[][] resultMatrix = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                resultMatrix[i][j] = random.nextInt(bounds) + offset;
+            }
+        }
+        return resultMatrix;
+    }
+    //DO domu
+    public static String toBinary(int number) {
+        return null;
+    }
+    //DO domu
+    public static int toNumber(String binaryCode) {
+        return 0;
+    }
+    //DO domu
+    //#FF0050 -> {255, 0, 80}
+    public static int[] rgbToValues(String rgb) {
+        return null;
+    }
+
+    //01234567
+    //Ala ma kota, kota -> 7
+    public static int find(String message, String sentence) {
+        return 0;
+    }
+
+    //Ala ma kota i ma tez psa, ma -> 2
+    public static int countAll(String message, String sentence) {
+        return 0;
+    }
+
+    //((2+2)*2) -> true
+    //(2+2)*2) -> false
+    //)(2+2)*2 -> false
+    public static boolean checkRoundBraces(String expression) {
+        return true;
     }
 }
